740acef95004a0ca667ac125b80a391a88e4cbcc
Nathan <nathanyu.tw+github+test@gmail.com>
2021-01-31 18:21:12 +1100

    Q1 solution

---
+++ AgentBox.php
@@ -0,0 +1,19 @@
+<?php
+declare(strict_types=1);
+class Q1
+{
+    public function repeatWord(string $str, bool $caseSensitive=true) : bool
+    {
+        $res = [];
+        for ($i=0; $i < strlen($str); $i++) {
+            if (!in_array($str[$i], $res)) {
+                if (ctype_alnum($str[$i])) {
+                    $res[] = $caseSensitive ? $str[$i] : strtolower($str[$i]);
+                }
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+}
